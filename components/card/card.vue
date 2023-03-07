d24af869ca67cfacfc897295aee4b5232ef43fbd
wenyechuang <126648592+wenyechuang@users.noreply.github.com>
2023-03-07 11:34:04 +0800

    Add files via upload

---
+++ components/card/card.vue
@@ -0,0 +1,85 @@
+<template>
+	<view class="driver">
+		<view class="box">
+			<view class="number">川A66666</view>
+			<view>
+				<image src="../../static/Rectangle 48.png" class="Rectangle"></image>
+			</view>
+		</view>
+		<view class="line"></view>
+		<view class="box">
+			<view class="search">选择司机</view>
+			<view style="display: flex;align-items: center;">
+				<view class="search">请选择</view>
+				<image src="../../static/arrow-right 1.png" class="arrow-right"></image>
+			</view>
+		</view>
+		<view class="contact">
+			<view class="search">联系方式</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.driver{
+		width: 690rpx;
+		height: 285rpx;
+		background: #FFFFFF;
+		border-radius: 20rpx 20rpx 20rpx 20rpx;
+		opacity: 1;
+		margin: 20rpx 25rpx;
+		overflow: hidden;
+	}
+	.box{
+		width: 625rpx;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 30rpx 25rpx;
+	}
+	.number{
+		width: 138rpx;
+		height:42rpx;
+		font-size: 30rpx;
+		color: #333333;
+	}
+	.Rectangle{
+		width: 30rpx;
+		height:30rpx
+	}
+	.line{
+		width: 625rpx;
+		height: 1rpx;
+		background: #D9D9D9;
+		border-radius: 0rpx 0rpx 0rpx 0rpx;
+		opacity: 1;
+		margin: 30rpx 25rpx;
+	}
+	.search{
+		/* width:96rpx; */
+		hieght:34rpx;
+		font-size: 24rpx;
+		color: #999999;
+	}
+	.arrow-right{
+		width: 24rpx;
+		height:24rpx;
+		margin-left: 3rpx;
+	}
+	.contact{
+		margin: 34rpx 25rpx;
+	}
+</style>
---
+++ components/plan/plan.vue
@@ -0,0 +1,112 @@
+<template>
+	<view class="lists">
+		<view class="address">成都双流-曲靖宣威</view>
+		<view class="content">砂石 袋装 500吨</view>
+		<view class="progress-show">
+			<view class="progress">已接单335吨</view>
+			<text class="number">45%</text>
+		</view>
+		<view class="line"></view>
+		<view class="allBtn">
+			<view class="btn">关闭</view>
+			<view class="btn">调度记录</view>
+			<view class="btn" @click="skip">指派</view>
+			<view class="btn" @click="share">分享</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			skip(){
+				uni.navigateTo({
+					url:"/pages/appoint/appoint"
+				})
+			},
+			share(){
+				uni.navigateTo({
+					url:"/pages/share/share"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+.lists {
+		// position: absolute;
+		overflow: hidden;//解决塌陷
+		
+		width: 690rpx;
+		height: 267rpx;
+		background: #FFFFFF;
+		margin: 30rpx;
+		border-radius: 20rpx;
+		margin-top: 20rpx;
+	}
+
+	.address {
+		font-size: 32rpx;
+		margin-top: 20rpx;
+		margin-left: 30rpx;
+	}
+
+	.content {
+		font-size: 24rpx;
+		color: #999999;
+		margin-top: 10rpx;
+		margin-left: 30rpx;
+	}
+
+	.progress-show {
+		display: flex;
+		font-size: 12rpx;
+		margin-top: 20rpx;
+		margin-left: 30rpx;
+	}
+
+	.progress {
+		width: 141rpx;
+		height: 21rpx;
+		background: #0982FA;
+		color: white;
+		border-radius: 10rpx;
+	}
+
+	.number {
+		color: #0982FA;
+		margin-left: 10rpx;
+	}
+
+	.line {
+		width: 630rpx;
+		height: 0.99rpx;
+		background: #D9D9D9;
+		margin-top: 20rpx;
+		margin-left: 30rpx;
+	}
+
+	.allBtn {
+		display: flex;
+	}
+
+	.btn {
+		width: 140rpx;
+		height: 56rpx;
+		background: #0982FA;
+		border-radius: 10rpx;
+		font-size: 24rpx;
+		color: #FFFFFF;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 20rpx;
+		margin-left: 30rpx;
+	}
+</style>
